1d4bb47c0d46432aadbf6909b58afdc2bdab297e
Cynthia <cynthia.dlatorre@gmail.com>
2020-07-10 20:11:21 -0500

    Mi primer proyecto en git

---
+++ script.js
@@ -0,0 +1,15 @@
+function sumar(){
+	alert("la suma de 50 + 45 =" + (50+45))
+}
+function cambiaColor(){
+	document.getElementById('cuerpo').style.backgroundColor='pink';
+}
+function cambiaColor2(colorElegido){
+	document.getElementById('cuerpo').style.backgroundColor=colorElegido;
+}
+function calcular(){
+	var nro1 = document.getElementById("nro1").value;
+	var nro2 = document.getElementById("nro2").value;
+	var suma = parseInt(nro1) + parseInt(nro2);
+	alert(suma)
+}
